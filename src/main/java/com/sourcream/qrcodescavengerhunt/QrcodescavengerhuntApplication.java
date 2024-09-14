b70b05af9a07c0bc832724d20496d31e017208c1
Lunga Mbatha <51107227+lungashandu@users.noreply.github.com>
2024-09-14 20:22:15 +0200

    initial commit

---
+++ src/main/java/com/sourcream/qrcodescavengerhunt/QrcodescavengerhuntApplication.java
@@ -0,0 +1,13 @@
+package com.sourcream.qrcodescavengerhunt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrcodescavengerhuntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrcodescavengerhuntApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sourcream/qrcodescavengerhunt/QrcodescavengerhuntApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sourcream.qrcodescavengerhunt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrcodescavengerhuntApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
