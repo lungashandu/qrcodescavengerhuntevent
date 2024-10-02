79c490631c50fc69d572a470fd80719e18846422
Lunga Mbatha <51107227+lungashandu@users.noreply.github.com>
2024-10-02 13:41:36 +0200

    user repository tests, initial commit

--- src/main/java/com/sourcream/qrcodescavengerhunt/domain/entities/UserEntity.java
+++ src/main/java/com/sourcream/qrcodescavengerhunt/domain/entities/UserEntity.java
@@ -11,7 +11,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 @Entity
-@Table(name = "users", uniqueConstraints = {@UniqueConstraint(columnNames = "username"),
+@Table(name = "users", uniqueConstraints = {@UniqueConstraint(columnNames = "sub"),
                                             @UniqueConstraint(columnNames = "email")
 })
 public class UserEntity {
---
+++ src/test/java/com/sourcream/qrcodescavengerhunt/repositories/UserEntityRepositoryIntegrationTests.java
@@ -0,0 +1,4 @@
+package com.sourcream.qrcodescavengerhunt.repositories;
+
+public class UserEntityRepositoryIntegrationTests {
+}
