84ec8508c97c5c75e1124f0990f7352cd2fe0c36
Lunga Mbatha <51107227+lungashandu@users.noreply.github.com>
2024-10-02 10:39:44 +0200

    user service, initial commit

---
+++ src/main/java/com/sourcream/qrcodescavengerhunt/services/UserService.java
@@ -0,0 +1,18 @@
+package com.sourcream.qrcodescavengerhunt.services;
+
+import com.sourcream.qrcodescavengerhunt.domain.entities.Role;
+import com.sourcream.qrcodescavengerhunt.domain.entities.UserEntity;
+
+import java.util.Optional;
+import java.util.Set;
+
+public interface UserService {
+
+    UserEntity registerUser(UserEntity user);
+
+    Optional<UserEntity> getUserByEmail(String email);
+
+    UserEntity updateUserRole(Long id, Set<Role> role);
+
+    void deleteUser(Long id);
+}
---
+++ src/main/java/com/sourcream/qrcodescavengerhunt/services/impl/UserServiceImpl.java
@@ -0,0 +1,4 @@
+package com.sourcream.qrcodescavengerhunt.services.impl;
+
+public class UserServiceImpl {
+}
