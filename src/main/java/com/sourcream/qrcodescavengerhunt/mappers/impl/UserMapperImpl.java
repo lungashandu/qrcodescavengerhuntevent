791491108f548b76bc796e1b4819e7857d7185b7
Lunga Mbatha <51107227+lungashandu@users.noreply.github.com>
2024-10-07 18:38:47 +0200

    model mapper

---
+++ src/main/java/com/sourcream/qrcodescavengerhunt/mappers/impl/UserMapperImpl.java
@@ -0,0 +1,25 @@
+package com.sourcream.qrcodescavengerhunt.mappers.impl;
+
+import com.sourcream.qrcodescavengerhunt.domain.dto.UserDto;
+import com.sourcream.qrcodescavengerhunt.domain.entities.UserEntity;
+import com.sourcream.qrcodescavengerhunt.mappers.Mapper;
+import org.modelmapper.ModelMapper;
+
+public class UserMapperImpl implements Mapper<UserEntity, UserDto> {
+
+    private ModelMapper modelMapper;
+
+    public UserMapperImpl(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
+    @Override
+    public UserDto mapTo(UserEntity userEntity) {
+        return modelMapper.map(userEntity, UserDto.class);
+    }
+
+    @Override
+    public UserEntity mapFrom(UserDto userDto) {
+        return modelMapper.map(userDto, UserEntity.class);
+    }
+}
